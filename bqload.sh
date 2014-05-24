dac81b2eef0768a2dd326aef282e9e7b77c1d65d
Ralph Yozzo <fedex1@gmail.com>
2014-05-24 18:27:08 -0400

    add proper bq loader

--- bqload.sh
+++ bqload.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 # participantsCount,end,name,tags,lat,lng,description,start,location,gplusEventUrl,defaultEventUrl
 # participantsCount:integer,end:timestamp,name:string,tags:string,lat:string,lng:string,description:string,start:timestamp,location:string,gplusEventUrl:string,defaultEventUrl
-bq load gdg.event_history all.gdg.2.csv participantsCount:integer,end:timestamp,name:string,tags:string,lat:string,lng:string,description:string,start:timestamp,location:string,gplusEventUrl:string,defaultEventUrl 
+# bq load gdg.event_history all.gdg.2.csv participantsCount:integer,end:timestamp,name:string,tags:string,lat:string,lng:string,description:string,start:timestamp,location:string,gplusEventUrl:string,defaultEventUrl 
+bq load gdg.event_history all.gdg.2.post.process.csv end:timestamp,name:string,tags:string,participantsCount:integer,start:timestamp,location:string,lat:float,lng:float,defaultEventUrl:string,gplusEventUrl:string,description
+# end,name,tags,participantsCount,start,location,lat,lng,defaultEventUrl,gplusEventUrl,description
+# end,name,tags,participantsCount,start,location,lat,lng,defaultEventUrl,gplusEventUrl,description
+# end:timestamp,name:string,tags:string,participantsCount:integer,start:timestamp,location:string,lat:float,lng:float,defaultEventUrl:string,gplusEventUrl:string,description

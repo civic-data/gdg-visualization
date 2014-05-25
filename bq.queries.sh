59abac1f66ceb44054ad6e484cc04c23dd3d043f
Ralph Yozzo <fedex1@gmail.com>
2014-05-24 22:39:49 -0400

    add more queries

--- bq.queries.sh
+++ bq.queries.sh
@@ -21,7 +21,45 @@ set -x
 # bq query "SELECT now()"
 # bq query "SELECT current_timestamp()"
 # bq query "SELECT DATE_ADD(current_timestamp(), -6, 'MONTH')"
-bq query "SELECT count(*) FROM [gdg.event_history] where start > DATE_ADD(current_timestamp(), -6, 'MONTH')"
-bq query "SELECT count(*) FROM [gdg.event_history] where start < DATE_ADD(current_timestamp(), -6, 'MONTH')"
+# bq query "SELECT count(*) FROM [gdg.event_history] where start > DATE_ADD(current_timestamp(), -6, 'MONTH')"
+# bq query "SELECT count(*) FROM [gdg.event_history] where start < DATE_ADD(current_timestamp(), -6, 'MONTH')"
 # bq query "SELECT count(*),year(start) FROM [gdg.event_history] group by year(start) order by 1 desc"
-bq query "SELECT count(*),year(start) as group1 FROM [gdg.event_history] group by group1 order by group1 desc"
+# bq query "SELECT count(*),year(start) as group1 FROM [gdg.event_history] group by group1 order by group1 desc"
+bq query "SELECT count(*),STRFTIME_UTC_USEC(UTC_USEC_TO_MONTH(TIMESTAMP_TO_USEC(start)),'%Y-%m-%d') as group1 FROM [gdg.event_history] group by group1 order by group1 desc"
+# Bucketing Results by Timestamp  
+# It's useful to use date and time functions to group query results into buckets corresponding to particular years, months, or days. The following example uses the UTC_USEC_TO_MONTH() function to display how many characters each Wikipedia contributor uses in their revision comments per month.
+# 
+# SELECT
+# contributor_username,
+# /* Return the timestamp shifted to the
+# * start of the month, formatted in
+# * a human-readable format. Uses the
+# * 'LEFT()' string function to return only
+# * the first 7 characters of the formatted timestamp.
+# */
+# LEFT (FORMAT_UTC_USEC(
+# UTC_USEC_TO_MONTH(timestamp * 1000000)),7) 
+# AS month,
+# SUM(LENGTH(comment)) as total_chars_used
+# FROM
+# [publicdata:samples.wikipedia]
+# WHERE
+# (contributor_username != '' AND 
+# contributor_username IS NOT NULL)
+# AND timestamp > 1133395200
+# AND timestamp < 1157068800
+# GROUP BY
+# contributor_username, month
+# ORDER BY
+# total_chars_used DESC;
+# Returns (truncated):
+# 
+# +--------------------------------+---------+-----------------------+
+# |      contributor_username      |  month  | total_chars_used      |
+# +--------------------------------+---------+-----------------------+
+# | Kingbotk                       | 2006-08 |              18015066 |
+# | SmackBot                       | 2006-03 |               7838365 |
+# | SmackBot                       | 2006-05 |               5148863 |
+# | Tawkerbot2                     | 2006-05 |               4434348 |
+# | Cydebot                        | 2006-06 |               3380577 |
+# etc ...

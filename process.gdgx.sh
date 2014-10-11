d70d2885dabc96f6d5f77b8a26698cddc592f803
Ralph Yozzo <fedex1@gmail.com>
2014-10-11 19:40:58 -0400

    add update

--- process.gdgx.sh
+++ process.gdgx.sh
@@ -2,3 +2,4 @@
 # curl "https://hub.gdgx.io/api/v1/events?perpage=9999999" | ./process.json.py
 # curl "https://hub.gdgx.io/api/v1/chapters?perpage=9999999" | ./process.json.py
 curl "https://hub.gdgx.io/api/v1/chapters/102033742326416578905/events?perpage=999999999" | ./process.json.py
+#      https://hub.gdgx.io/api/v1/chapters/102033742326416578905/events?perpage=9999
